85e714aedb785df6be91785bc87925ee855f3ac9
Jackiets98 <100361056+Jackiets98@users.noreply.github.com>
2024-03-07 13:30:56 +0800

    updated voice chat, and currently working

--- lib/user/fragment/ReportFragment.dart
+++ lib/user/fragment/ReportFragment.dart
@@ -1,7 +1,13 @@
+import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_sound/public/flutter_sound_player.dart';
+import 'package:flutter_sound/public/flutter_sound_recorder.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:social_media_recorder/audio_encoder_type.dart';
 import '../../main/utils/Constants.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -30,6 +36,95 @@ class _ReportFragmentState extends State<ReportFragment> with WidgetsBindingObse
   TextEditingController _messageController = TextEditingController();
   bool _isTyping = false; // Flag to indicate if the user is typing
   double _keyboardPadding = 50.0; // Initial padding value
+  bool isRecorderReady = false;
+  bool isPlaying = false;
+  final _recorder = FlutterSoundRecorder();
+  late String _recordFilePath;
+  final _player = FlutterSoundPlayer();
+  Timer? _timer;
+  int _elapsedSeconds = 0;
+
+  void _startTimer() {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        _elapsedSeconds++;
+      });
+    });
+  }
+
+  void _stopTimer() {
+    _timer?.cancel();
+    _elapsedSeconds = 0;
+  }
+
+
+
+  Future initRecorder() async{
+    final status = await Permission.microphone.request();
+
+    if (status != PermissionStatus.granted) {
+      throw 'Micrphone Permission Is Not Granted';
+    }
+
+    await _recorder.openRecorder();
+    isRecorderReady = true;
+    _recorder.setSubscriptionDuration(const Duration(microseconds: 500));
+  }
+
+  Future record() async{
+    if (!isRecorderReady) return;
+
+    // Get the external storage directory
+    final directory = await getExternalStorageDirectory();
+    _recordFilePath = '${directory!.path}/audio.aac';
+
+    _startTimer();
+
+    await _recorder.startRecorder(toFile: _recordFilePath);
+  }
+
+
+  Future stop() async{
+    if (!isRecorderReady) return;
+
+    final path = await _recorder.stopRecorder();
+    final audioFile = File(path!);
+
+    _stopTimer();
+    _saveRecordingToBackend(path);
+
+    print('Recorded audio: $path');
+  }
+
+  Future<void> play() async {
+    print('Play function called');
+    if (!await File(_recordFilePath).exists()) {
+      print('No recorded file found');
+      return;
+    }
+
+    try {
+      print('Opening player');
+      await _player.openPlayer().whenComplete(() {
+        isPlaying = true;
+      });
+      await _player.startPlayer(fromURI: _recordFilePath).whenComplete(() {
+        setState(() {
+          isPlaying = false;
+        });
+      });
+    } catch (e) {
+      print('Failed to play recording: $e');
+    }
+  }
+
+  Future<void> stopPlayback() async {
+    try {
+      await _player.pausePlayer();
+    } catch (e) {
+      print('Failed to stop playback: $e');
+    }
+  }
 
   @override
   void initState() {
@@ -42,11 +137,15 @@ class _ReportFragmentState extends State<ReportFragment> with WidgetsBindingObse
       print('User Name: $userName');
       print('Android ID: $androidID');
     });
+    initRecorder();
   }
 
   @override
   void dispose() {
     WidgetsBinding.instance?.removeObserver(this);
+    _player.closePlayer();
+    _recorder.closeRecorder();
+    _stopTimer();
     super.dispose();
   }
 
@@ -236,6 +335,8 @@ class _ReportFragmentState extends State<ReportFragment> with WidgetsBindingObse
       } else {
         // Handle the error
         print('Failed with status code: ${response.statusCode}');
+        // Print out the response body for more information
+        print(await response.stream.bytesToString());
       }
     } catch (e) {
       print('Error: $e');
@@ -465,21 +566,53 @@ class _ReportFragmentState extends State<ReportFragment> with WidgetsBindingObse
                   _isRecording
                       ? Column(
                     children: [
-                      SocialMediaRecorder(
-                        startRecording: () {
-                          // setState(() {
-                          //   _isRecording = true;
-                          // });
-                        },
-                        stopRecording: (_time) {
-                          // setState(() {
-                          //   _isRecording = false;
-                          // });
-                        },
-                        sendRequestFunction: (soundFile, _time) {
-                          _saveRecordingToBackend(soundFile.path);
-                        },
-                        encode: AudioEncoderType.AAC,
+                      Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('${_elapsedSeconds ~/ 60}:${(_elapsedSeconds % 60).toString().padLeft(2, '0')}'),
+                              SizedBox(height: 10,),
+                              ElevatedButton(
+                                child: Icon(
+                                  _recorder.isRecording ? Icons.stop : Icons.mic,
+                                  size: 80,
+                                ),
+                                onPressed: () async{
+                                  if (_recorder.isRecording) {
+                                    await stop();
+                                  }else {
+                                    await record();
+                                  }
+
+                                  setState(() {
+
+                                  });
+                                },
+                              ),
+                              SizedBox(height: 50,),
+                              ElevatedButton(
+                                onPressed: () async {
+                                  if (isPlaying) {
+                                    await stopPlayback(); // Pause the audio playback
+                                  } else {
+                                    if (_player.isPaused) {
+                                      await _player.resumePlayer(); // Resume the playback
+                                    } else {
+                                      await play(); // Start playing the audio
+                                    }
+                                  }
+                                  setState(() {
+                                    // Toggle the isPlaying state
+                                    isPlaying = !isPlaying;
+                                  });
+                                },
+                                child: Icon(
+                                  isPlaying ? Icons.stop : Icons.play_arrow,
+                                  size: 80,
+                                ),
+                              ),
+                            ],
+                          )
                       ),
                       SizedBox(height: 16.0),
                       IconButton(
